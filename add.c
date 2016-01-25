9545ed4867e5a35132314f53fdbdd3e15162e43e
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-25 15:14:03 -0500

    exercise

---
+++ add.c
@@ -0,0 +1,19 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    int a;
+    int b;
+    int result;
+     
+    printf("enter first int:\n");
+    scanf("%d",&a);
+    printf("enter second int:\n");
+    scanf("%d" ,&b);
+    result = a+b;
+    printf("%d plus %d equals %d\n", a, b,result);
+         
+}
