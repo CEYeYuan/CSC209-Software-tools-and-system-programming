0cb9a555aef43b695b9239c62d039ebca9284936
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-30 20:36:35 -0500

    almost done

--- c4yuanyf/A1/hogs.c
+++ c4yuanyf/A1/hogs.c
@@ -4,64 +4,75 @@
  
 int main(int argc,char **argv)
 {
-    int i,j;
+    int i,j,inited,done,line;
     char tmp[11][256];//buffer for each line
     char max[11][256];//buffer for the process with max usage
-    char first_line[256];//buffer for the first line. Useless since no actual data
+    strcpy(max[2],"-1");
+    strcpy(max[3],"-1");
     char buffer[256];
-    int done=0;
+    inited=done=line=0;
+    
+    if(argc<=1||argc>=4){
+    	printf("%s\n","too few/much argument" );
+    	printf("%s\n","Correct usage: hogs -m/-u username" );
+    	return -1;//error
+    }
 
-
-    int line=0;
     while(1){
     	if(done)
-	    	break;
-    	//we only wants the useful data, so skip line 1
-    	if(line==0){
-    	//	printf("here\n");
-    		for(j=0;j<11;j++)
-    			scanf("%s",first_line);
-    	}
+			break;
+	    //we only wants the useful data, so skip line 1
+	   	if(line==0){
+	   		for(j=0;j<11;j++)
+	   			scanf("%s",buffer);
+		}
+	    
+	    else{
+		    for(j=0;j<11;j++){
+		   		int num_char;
+		   		num_char=scanf("%s",tmp[j]);
+		   		if(num_char<=0){
+		   			done=1;
+		    		break;
+		    	}
+		   	}
 
-    	if(line==1){
-    		for(j=0;j<11;j++)
-    			scanf("%s",max[j]);
-    	}
-    	else{
-    	
-	    	for(j=0;j<11;j++){
-	    		int num_char;
-	    		num_char=scanf("%s",tmp[j]);
-	    		if(num_char<=0){
-	    			done=1;
-	    			break;
+		   	if(argc==2){
+		   		if(strtol(tmp[2],NULL,10)>strtol(max[2],NULL,10)&&strcmp(tmp[0],argv[1])==0){
+					inited=1;
+	   				for(j=0;j<11;j++){
+	   					strcpy(max[j],tmp[j]);
+		   			}
 	    		}
-	    		//printf("argc=%d argv[1]=%s\n",argc,argv[1] );
-	    		//if(argc==3){
-	    		//	printf("arv[1]=%s\n",argv[1] );
-	    		//	if(argv[1]=="-m")
-	    		//		printf("%s\n","equal" );
-	    			if(strtol(tmp[3],NULL,10)>strtol(max[3],NULL,10)){
-	    				for(j=0;j<11;j++){
-	    					strcpy(max[j],tmp[j]);
-	    				}
+	    	}	
+		    if(argc==3){
+		   		if(strcmp(argv[1],"-m")==0&&strcmp(tmp[0],argv[2])==0){
+		   			if(strtof(tmp[3],NULL)>strtof(max[3],NULL)){
+		   				inited=1;
+			   			for(j=0;j<11;j++){
+			   				strcpy(max[j],tmp[j]);
+		   				}
+		   			}
+	    		}
+
+	    		if(strcmp(argv[1],"-c")==0&&strcmp(tmp[0],argv[2])==0){
+	    			if(strtof(tmp[2],NULL)>strtof(max[2],NULL)){
+	    				inited=1;
+		   				for(j=0;j<11;j++){
+		   					strcpy(max[j],tmp[j]);
+			   			}
 	    			}
-	    		//}
-	    		/*if((argc==2)||(argc==3&&argv[2]=="-c")){
-	    			if(strtol(tmp[2])>strtol(max[2])){
-	    				for(j=0;j<11;j++){
-	    					max[j]=tmp[j];
-	    				}
-	    			}
-	    		}*/
-	    		//printf("%s ",tmp[j]);
-	    	}
-	    	
-	    	//printf("\n");
-    	}
-    	line++;
-    } 
-    for(j=0;j<11;j++){
-	    printf("%s ",max[j]);
-	} 
+	    		}
+
+		    }
+	    }
+		line++;
+   } 
+   if(inited==1){
+   		if(argc==3&&strcmp(argv[1],"m")==0)
+   			printf("%s\t%.1f\t%s\n", max[1],strtof(max[3],NULL),max[10]);
+   		else
+   			printf("%s\t%.1f\t%s\n", max[1],strtof(max[2],NULL),max[10]);
+    }
+   
 }
