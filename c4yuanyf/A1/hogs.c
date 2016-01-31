b00f391e0a220e40ee6bf362ce4f4a9b3821ef64
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-30 19:41:19 -0500

    almost done

--- c4yuanyf/A1/hogs.c
+++ c4yuanyf/A1/hogs.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
  
 int main(int argc,char **argv)
 {
     int i,j;
-    char *tmp[11];//buffer for each line
-    char *max[11];//buffer for the process with max usage
-    char *flag[11];
-    char first_line[128];//buffer for the first line. Useless since no actual data
+    char tmp[11][256];//buffer for each line
+    char max[11][256];//buffer for the process with max usage
+    char first_line[256];//buffer for the first line. Useless since no actual data
+    char buffer[256];
     int done=0;
 
-    for(i=0;i<11;i++){
-    	tmp[i]=malloc(sizeof(char)*128);
-    	max[i]=malloc(sizeof(char)*128);
-    }
 
     int line=0;
     while(1){
@@ -24,6 +19,11 @@ int main(int argc,char **argv)
     	//we only wants the useful data, so skip line 1
     	if(line==0){
     	//	printf("here\n");
+    		for(j=0;j<11;j++)
+    			scanf("%s",first_line);
+    	}
+
+    	if(line==1){
     		for(j=0;j<11;j++)
     			scanf("%s",max[j]);
     	}
@@ -37,18 +37,16 @@ int main(int argc,char **argv)
 	    			break;
 	    		}
 	    		//printf("argc=%d argv[1]=%s\n",argc,argv[1] );
-	    		if(argc==3){
+	    		//if(argc==3){
 	    		//	printf("arv[1]=%s\n",argv[1] );
-	    			if(argv[1]=="-m")
-	    				printf("%s\n","equal" );
-	    			if(tmp[0]==argv[2]&&strtol(tmp[3],NULL,10)>strtol(max[3],NULL,10)){
+	    		//	if(argv[1]=="-m")
+	    		//		printf("%s\n","equal" );
+	    			if(strtol(tmp[3],NULL,10)>strtol(max[3],NULL,10)){
 	    				for(j=0;j<11;j++){
-	    					flag[j]=tmp[j];
-	    					tmp[j]=max[j];
-	    					max[j]=flag[j];
+	    					strcpy(max[j],tmp[j]);
 	    				}
 	    			}
-	    		}
+	    		//}
 	    		/*if((argc==2)||(argc==3&&argv[2]=="-c")){
 	    			if(strtol(tmp[2])>strtol(max[2])){
 	    				for(j=0;j<11;j++){
@@ -63,7 +61,7 @@ int main(int argc,char **argv)
     	}
     	line++;
     } 
-    /*for(j=0;j<11;j++){
+    for(j=0;j<11;j++){
 	    printf("%s ",max[j]);
-	} */     
+	} 
 }
